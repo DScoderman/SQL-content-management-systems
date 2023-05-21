7dee8f33f49f71ff68e3d98d0619eca129f4ee0b
DSCoderman <dsenour6@gmail.com>
2023-05-21 15:53:56 -0400

    add employee function added

--- index.js
+++ index.js
@@ -2,20 +2,148 @@ const cTable = require('console.table');
 const inquirer = require('inquirer')
 const questions = require('./questions');
 const mysql = require('mysql2');
-const querySearch = require('./db/DB.js')
+
+const { intro, addDepartment, addRole, updateEmployeeInfo } = require("./questions");
 
 const db = mysql.createConnection({
     host: "localhost",
-    user:"root",
+    user: "root",
     password: 'Drexel2021!',
     database: 'company_db'
 });
-db.connect(err=>{
-    if(err) throw err;
-}); 
+db.connect(err => {
+    if (err) throw err;
+    init();
+});
 
-function init = () => viewOptions()
+function init() {
+    inquirer.prompt(intro)
+        .then(answers => {
+            console.log(answers)
+            if (answers.task === 'view_departments') {
+                viewDept()
+            }
+            else if(answers.task === 'add_department') {
+                addDepartment()
+            }
+            else if (answers.task === 'add_employee') {
+                addEmployee()
+            }
+            else if (answers.task === 'add_role') {
+                addRole()
+            }
+            else if (answers.task === 'view_roles') {
+            viewRoles()
+            }
+            else if (answers.task === 'update_employee'){
+            updateEmployeeInfo()
+            }
+            else if (answers.task === 'view_employees'){
+                viewEmployee()
+            }
+        });
+}
 
-const viewOptions =
+function viewDept() {
+    const sqlString = `
+    SELECT *
+    FROM department`;
+    db.query(sqlString, (err, data) => {
+        if (err) throw err;
+        console.table(data)
+        init();
+    })
+}
 
- 
+function viewRoles() {
+    const sqlString = 
+    'SELECT role.title AS "Job Title", role.id AS "Role ID", department.name AS "Department", role.salary AS "Salary" FROM role JOIN department ON role.department_id = department.id';
+
+    db.query(sqlString, (err, data) => {
+        if (err) throw err;
+        console.table(data)
+        init();
+    })
+}
+
+function viewEmployee(){
+    const sqlString= "SELECT employee.id AS `Employee ID`, employee.first_name AS `First Name`, employee.last_name AS `Last Name`, role.title AS `Job Title`, department.name AS `Department`, role.salary AS `Salary`, CONCAT(manager.first_name, ' ', manager.last_name) AS `Manager` FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;";
+    
+    db.query(sqlString, (err, result) => {
+      
+        if (err) {
+            throw err
+        }
+        console.table(result)
+        
+    })
+}
+
+
+function addEmployee() {
+    db.query('SELECT * FROM ROLE', function (err, roleResults) {
+        if(err) {
+            throw err
+        }
+    db.query('SELECT * FROM employee WHERE manager_id IS NULL', function (err, employeeResults) {
+        var employees= employeeResults.map(employee => `${employee.first_name} ${employee.last_name}`)
+        employees.push("");
+        console.log(employees)
+
+    inquirer .prompt([
+        {
+            type: 'input',
+            message: 'What is the first name?',
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: 'What is the last name?',
+            name: 'lastName'
+        },
+        {
+            type: 'rawlist',
+            message: 'role?',
+            name: 'employeeRole',
+            choices: roleResults.map(role => `${role.title}`)
+        },
+        {
+            type: 'rawlist',
+           message: 'Who is their manager?',
+           name: 'manager',
+           choices: employees
+         }
+    ]).then (answer => {
+        const firstName = answer.firstName;
+        const lastName = answer.lastName;
+        const role = answer.employeeRole;
+        const manager = answer.manager === "" ? undefined : answer.manager;
+
+        db.query(`SELECT id FROM role WHERE title =?`, role, (err, results) => {
+            if (err) {
+                throw err
+            }
+            const roleID = results[0].id
+
+        db.query(`SELECT id FROM employee WHERE CONCAT (first_name, " ", last_name) = ?`, manager, (err, managerResults) => {
+            if (err) {
+                throw err
+            }
+            const managerID = managerResults[0].id;
+
+        db.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [firstName, lastName, roleID, managerID], (err, results) => {
+                    if (err) {
+                        throw err
+                    }
+                    //alerts user if the new employee was added successfully
+                    console.log('Added new employee!');
+                    //calls promptList function so it immediately repopulates under success notice
+                    init();
+             });
+           });
+        });
+      });
+    });
+  });
+};
+         
--- questions.js
+++ questions.js
@@ -1,75 +1,77 @@
 
 const intro = [
-{
-    type: "list",
-    message: "what would you like to do?",
-    name: "task",
-    choices: [ 'view_departments', 'view_roles', 'view_employees', 'add_employee', 'add_role', 'add_department', 'update_employee']
-},
+    {
+        type: "list",
+        message: "what would you like to do?",
+        name: "task",
+        choices: ['view_departments', 'view_roles', 'view_employees', 'add_employee', 'add_role', 'add_department', 'update_employee',]
+    },
 ];
 
 const addDepartment = [
     {
-    type: "input",
-    message: "Please enter a new department name.",
-    Name: "newDeptName",
-}
+        type: "input",
+        message: "Please enter a new department name.",
+        name: "newDeptName",
+    }
 ];
 const addRole = [
     {
-    type: "input",
-    message: "Please enter a new role title.",
-    Name: "newRoleTitle",
-},
-{
-    type: "input",
-    message: "Please enter the salary of new role.",
-    Name: "newRoleSalary",
-},
-    {
-    type: "input",
-    message: "What department does this employee belong to?.",
-    Name: "newDeptName",
-}
-];
-const addEmployee = [
-    {
-    type: "input",
-    message: "What is the employee's first name?",
-    Name: "newEmployeeFirstName",
-},
-{
-    type: "input",
-    message: "What is the employee's last name?",
-    Name: "newEmployeeLastName",
-},
-{
-    type: "input",
-    message: "What is the employee's role?",
-    Name: "newEmployeeRole",
-},
-{
         type: "input",
-        message: "What department does new employee belong to?",
-        Name: "newEmployeeDepartment",
-},
-{
-    type: "input",
-    message: "Who is the employee's manager?",
-    Name: "newEmployeeManager",
-},
+        message: "Please enter a new role title.",
+        name: "newRoleTitle",
+    },
+    {
+        type: "input",
+        message: "Please enter the salary of new role.",
+        name: "newRoleSalary",
+    },
+    {
+        type: "list",
+        message: "What department does this employee belong to?.",
+        name: "newDeptName",
+        choices: []
+
+    }
 ];
+// const addEmployee = [
+//     {
+//         type: "input",
+//         message: "What is the employee's first name?",
+//         name: "newEmployeeFirstName",
+//     },
+//     {
+//         type: "input",
+//         message: "What is the employee's last name?",
+//         name: "newEmployeeLastName",
+//     },
+//     {
+//         type: "input",
+//         message: "What is the employee's role?",
+//         name: "newEmployeeRole",
+//     },
+//     {
+//         type: "input",
+//         message: "What department does new employee belong to?",
+//         name: "newEmployeeDepartment",
+//     },
+//     {
+//         type: "input",
+//         message: "Who is the employee's manager?",
+//         name: "newEmployeeManager",
+//     },
+// ];
 
 const updateEmployeeInfo = [
-{
-    type: "input",
-    message: "select the employee you want to update.",
-    name: "employeeUpdate"
-},
-{
-    type: "input",
-    message: "select the employee you want to update.",
-    name: "employeeUpdate"
-},
+    {
+        type: "input",
+        message: "select the employee you want to update.",
+        name: "employeeUpdate"
+    },
+    {
+        type: "input",
+        message: "select the employee you want to update.",
+        name: "employeeUpdate"
+    },
 ];
-module.exports = {intro, addDepartment, addRole, addEmployee, updateEmployeeInfo}
+module.exports = { intro, addDepartment, addRole, updateEmployeeInfo }
